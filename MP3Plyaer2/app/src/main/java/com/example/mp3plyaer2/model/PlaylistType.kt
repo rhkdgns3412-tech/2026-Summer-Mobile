68b2706c0508063b2de8421dc3f5ed3dd74392b8
yun kwanghoon <rhkdgns3412@gmail.com>
2026-07-13 17:49:55 +0900

    Delete MP3Plyaer2 directory

--- MP3Plyaer2/app/src/main/java/com/example/mp3plyaer2/model/PlaylistType.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mp3plyaer2.model
-
-/** Types of playlists shown in the app. */
-enum class PlaylistType {
-    USER,
-    SMART_RECENT,
-    SMART_MOST_PLAYED
-}
-
--- MP3Plyaer2/app/src/main/java/com/example/mp3plyaer2/util/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mp3plyaer2.util
-
-import android.util.Log
-
-fun Any.logd(message: String) {
-    Log.d(this::class.java.simpleName, message)
-}
-
